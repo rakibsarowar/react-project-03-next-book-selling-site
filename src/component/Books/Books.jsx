d435de894e80fd77c9535f1c7bb58dd8ec959c5b
rakibsarowar <rakibsarowar@gmail.com>
2023-04-04 22:51:34 +0600

    added div and css

--- src/component/Books/Books.jsx
+++ src/component/Books/Books.jsx
@@ -18,8 +18,15 @@ const Books = () => {
         <div className='my-container'>
             <div className="grid gap-6 mb-8 lg:grid-cols-4 sm: grid-cols-2">
                 {books.map(book=>
-                <div className='overflow-hidden'>
-                    <img src={book.image} alt="book cover" className='object-cover' />
+
+                <div className='overflow-hidden relative transition duration-200 transform hover:-translate-y-2 rounded shadow-lg hover:shadow-2xl'>
+                    <img src={book.image} alt="book cover" className='object-cover w-full h-56 md: h-64 xl: h-80' />
+
+                    <div className='bg-black px-6 py-4 bg-opacity-75 opacity-0 hover:opacity-100 text-gray-300 absolute inset-0 transition-opacity duration-200 flex flex-col'>
+                        <p>{book.title}</p><br />
+                        <p>{book.subtitle.substring(0,45)}...</p><br />
+                        <p className='mt-auto'>Price: {book.price}</p>
+                    </div>
                 </div>
                 )}
             </div>
