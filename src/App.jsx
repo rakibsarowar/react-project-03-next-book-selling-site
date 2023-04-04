659a411ab7fce50867a2e1746b28f58df8569da0
rakibsarowar <rakibsarowar@gmail.com>
2023-04-04 16:59:46 +0600

    set up about page

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import { Outlet } from 'react-router-dom';
 const App = () => {
   return (
     <div>
-    <h1>This is App</h1>
-    {/* To show where children set , need to use place holder in below called OutLet. */}
-    <Outlet></Outlet>
+      {/* Header */}
+      <Outlet></Outlet>
+      {/* Footer */}
   </div>
   );
 };
