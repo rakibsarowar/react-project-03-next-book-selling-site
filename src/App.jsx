c0ad651edb92695b5f44e12ae1d1f449bd6bcac5
rakibsarowar <rakibsarowar@gmail.com>
2023-04-04 16:55:53 +0600

    router set up completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 const App = () => {
   return (
-    
-    <h1 className="text-3xl font-bold">
-      Hello world!
-    </h1>
+    <div>
+    <h1>This is App</h1>
+    {/* To show where children set , need to use place holder in below called OutLet. */}
+    <Outlet></Outlet>
+  </div>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,32 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './component/Home/Home'
+import About from './component/About/About'
+import Books from './component/Books/Books'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App></App>
-  },
+    // Parent  and need to call all children in it. 
+    element: <App></App>,
+    children:[
+      {
+        // 1st item will be default so this home will be default---------------------------------.
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path:'books',
+        element:<Books></Books>
+      },
 
-  {
-    path:'/about',
-    element: <p>Abuout page</p>
-  }
+      {
+        path: '/about',
+        element: <About></About>
+      }
+    ]
+  },
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
