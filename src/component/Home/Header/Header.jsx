e1a4934699e00a47e55b32ec7e6994e539bc32d1
rakibsarowar <rakibsarowar@gmail.com>
2023-04-04 17:37:26 +0600

    Step 11_set up Nav link and used active link colour in className

--- src/component/Home/Header/Header.jsx
+++ src/component/Home/Header/Header.jsx
@@ -1,17 +1,44 @@
 import React from 'react';
 import { BookOpenIcon } from '@heroicons/react/24/solid'
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div className='flex items-center justify-between'>
-            {/* logo section */}
+            {/* logo section---------------------- */}
             <Link to='/' className='inline-flex items-center' >
                 <BookOpenIcon className='h-6 w-6 text-blue-500'></BookOpenIcon>
                 <span className='ml-2 text-xl font-bold tracking-wide text-gray-800'> nextPage</span>
             </Link>
 
-            {/* Nav Item section */}
+            {/* Nav Item section------------------ */}
+            <ul className='items-center hidden space-x-8 lg:flex'>
+                <li>
+                    <NavLink
+                        to='/'
+                        className={({ isActive }) => (isActive ? 'text-blue-600' : 'default')}
+                    >
+                        Home
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to='/books'
+                        className={({ isActive }) => (isActive ? 'text-blue-600' : 'default')}
+                    >
+                        Books
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to='/about'
+                        className={({ isActive }) => (isActive ? 'text-blue-600' : 'default')}
+                    >
+                        About us
+                    </NavLink>
+                </li>
+            </ul>
+
         </div>
     );
 };
