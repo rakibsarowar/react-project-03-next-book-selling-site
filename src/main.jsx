83e43fdf40e62060ace70bc825002575fc2016ec
rakibsarowar <rakibsarowar@gmail.com>
2023-04-04 16:30:23 +0600

    setting up router in main jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App></App>
+  },
+
+  {
+    path:'/about',
+    element: <p>Abuout page</p>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  <RouterProvider router={router}></RouterProvider>
 )
