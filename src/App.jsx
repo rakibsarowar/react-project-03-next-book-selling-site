d7946d3808c4fee0935e819192caaad77db23e9a
rakibsarowar <rakibsarowar@gmail.com>
2023-04-04 17:26:26 +0600

    Icon set up completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Header from './component/Home/Header/Header';
 
 const App = () => {
   return (
     <div>
       {/* Header */}
+      <Header></Header>
       <Outlet></Outlet>
       {/* Footer */}
   </div>
--- src/component/Home/Header/Header.jsx
+++ src/component/Home/Header/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { BookOpenIcon } from '@heroicons/react/24/solid'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className='flex items-center justify-between'>
+            {/* logo section */}
+            <Link to='/' className='inline-flex items-center' >
+                <BookOpenIcon className='h-6 w-6 text-blue-500'></BookOpenIcon>
+                <span className='ml-2 text-xl font-bold tracking-wide text-gray-800'> nextPage</span>
+            </Link>
+
+            {/* Nav Item section */}
         </div>
     );
 };
